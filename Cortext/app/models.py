16ff4aafe23bd8cec5cc825fc8155d32291cb768
Daniel Har Even <daniel.hareven.40@gmail.com>
2019-12-09 21:17:26 +0200

    user and student added

--- Cortext/app/models.py
+++ Cortext/app/models.py
@@ -4,20 +4,24 @@ Definition of models.
 
 from django.db import models
 
+
+class User(models.Model):
+    username = models.CharField(max_length=20)
+    password = models.CharField(max_length=20)
+    id = models.CharField(max_length=9, primary_key=True)
+    first_name = models.CharField()
+    last_name = models.CharField()
+
+
 # Create your models here.
-class Student(models.Model):
-
-    username = models.CharField(max_length = 20)
-    password = models.CharField(max_length = 20)
-    type = models.IntegerField(default=0)
-
-    ordering = ['username']
-
-    def get_absolute_url(self):
-        return reverse('model-detail-view', args=[str(self.id)])
+class Student(User):
+    age_group = models.IntegerField()  # student's class
+    school = models.ForeignKey('School', models.CASCADE)
 
     def __str__(self):
-        return self.username
+        return self.first_name, self.last_name
+
+
 class School(models.Model):
     id = models.CharField(primary_key=True)
     name = models.CharField()
